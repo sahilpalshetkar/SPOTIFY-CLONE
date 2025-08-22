314b16640849bd444cbb13717c5f28c256fe3680
sahilpalshetkar <sahilpalshetkar125@gmail.com>
2025-08-22 23:20:07 +0530

    added functionality to playbar

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 console.log("let's write javaScript")
+let currentSong = new Audio();
+
+function secondsToMinutes(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let secs = Math.ceil(seconds % 60);
+
+  // Add leading zeros
+  let formattedMinutes = (minutes.toString().padStart(2, "0"));
+  let formattedSeconds = (secs.toString().padStart(2, "0"));
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/")
@@ -19,7 +32,17 @@ async function getSongs() {
 
 getSongs()
 
+const playMusic = (track)=>{
+    // let audio = new Audio("/songs/" + track)
+    currentSong.src = "/songs/" + track
+    currentSong.play()
+    play.src = "pause.svg"
+    document.querySelector(".songinfo").innerHTML = track
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
+
 async function main(){
+
     //Get the list of all songs
     let songs = await getSongs()
     console.log(songs)
@@ -29,8 +52,8 @@ async function main(){
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li><img src="music.svg" alt="">
                 <div class="info">
-                  ${song.replace("%5BOfficial_Music_Video%5D", "")}
-                  <div></div>
+                 <div>${song.replace("%5BOfficial_Music_Video%5D", "")}</div>
+                  <div>Sahil</div>
                 </div>
                 <div class="playnow">
                   <span>Play</span>
@@ -38,6 +61,39 @@ async function main(){
                 </div></li> `
     }
 
+    // Attach an event listner to each song
+   Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach((e)=>{
+    e.addEventListener("click", element=>{
+        console.log(e.querySelector(".info").firstElementChild.innerHTML)
+        playMusic(e.querySelector(".info").firstElementChild.innerHTML)
 
+    })
+   })
+
+   // Attach an event listener to play, next and previous
+   play.addEventListener("click", ()=>{
+    if (currentSong.paused) {
+        currentSong.play()
+        play.src = "pause.svg"
+    }
+    else{
+        currentSong.pause()
+        play.src = "play.svg"
+    }
+   })
+
+   // Listen for timeupdate event
+   currentSong.addEventListener("timeupdate", ()=>{
+    console.log(currentSong.currentTime, currentSong.duration)
+    document.querySelector(".songtime").innerHTML = `${secondsToMinutes(currentSong.currentTime)}/${secondsToMinutes(currentSong.duration)}`
+    document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+   })
+
+   // Add an event listener to seekbar
+   document.querySelector(".seekbar").addEventListener("click", e=>{
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left = percent + "%";
+    currentSong.currentTime = ((currentSong.duration) * percent) / 100
+   })
 }
 main()
